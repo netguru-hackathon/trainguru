8013f0f1466fe686c561968eac0b477c968e59f8
Dominik Wojciechowski <dominik.wojciechowski@netguru.co>
2015-08-21 12:38:12 +0200

    Nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events
-  resources :trains
-  resources :connections
+  resources :events do
+    resources :connections do
+      resources :trains
+    end
+  end
   root to: 'visitors#index'
 end
